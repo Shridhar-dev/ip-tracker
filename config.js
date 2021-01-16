94495cf9ef49c72e3d6323f73288fe57d9f08e3a
Shridhar-dev <shridharkamat10@gmail.com>
2021-01-16 19:03:23 +0530

    bug fixes

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {
+    IPIFY_KEY : 'uIc3HCAnoVTQYx5oCWLlRpgzyhWbq',
+    MAP_KEY : 'pk.eyJ1Ijoic2hyaWRoYXItZGV2IiwiYSI6ImNram12OXIwOTA3cHkydm84NTBwdHMzYmgifQ.L9kE174hVJKMY9b2kEsAQQ',
+  }
